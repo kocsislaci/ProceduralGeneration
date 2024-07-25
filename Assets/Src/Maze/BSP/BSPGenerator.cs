c2b190e3b645b40ff121d0f67e1895fc2d8bccb8
Imre Racz <imre@riltech.co>
2024-07-25 15:38:52 +0200

    feat(bsp): added basic particle slicing

--- Assets/Src/Maze/BSP/BSPGenerator.cs
+++ Assets/Src/Maze/BSP/BSPGenerator.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class BSPGenerator : Generator
 {
@@ -23,21 +26,60 @@ public class BSPGenerator : Generator
         return new Vector2Int(95, 95);
     }
 
+        private List<Particle> BreakDown(List<Particle> breakdown) {
+        List<Particle> breakdownList = new List<Particle>();
+        foreach (Particle particle in breakdown) { 
+            if (Math.Abs(particle.x0 - particle.x1) < 4) {
+                breakdownList.Add(particle);
+                continue;
+            };
+            int x = UnityEngine.Random.Range(particle.x0, particle.x1);
+            breakdownList.Add(new Particle(particle.x0, x, particle.y0, particle.y1));
+            breakdownList.Add(new Particle(x, particle.x1, particle.y0, particle.y1));
+        }
+
+        List<Particle> breakdownListFinal = new List<Particle>();
+        foreach (Particle particle in breakdownList) {
+            if (Math.Abs(particle.y0 - particle.y1) < 4) {
+                breakdownListFinal.Add(particle);
+                continue;
+            }
+            int y = UnityEngine.Random.Range(particle.y0, particle.y1);
+            breakdownListFinal.Add(new Particle(particle.x0, particle.x1, particle.y0, y));
+            breakdownListFinal.Add(new Particle(particle.x0, particle.x1, y, particle.y1));
+        }
+        return breakdownListFinal;
+    }
+
+    private void SliceMaze(Maze maze) {
+        var particles = new List<Particle>() {
+            new Particle(0, maze.Size, 0, maze.Size)
+        };
+
+        while (particles.Count < 1000) {
+            particles = BreakDown(particles);
+        }
+        
+        for(int i = 0; i < particles.Count; i++) {
+            if (i%2 == 0) continue;
+            var particle = particles[i];
+            for (int x = particle.x0; x < particle.x1; x++)
+            {
+                for (int y = particle.y0; y < particle.y1; y++)
+                {
+                    maze.Set(x, y, true);
+                }
+            }
+        }
+    }
+
+
     public override Maze GenerateMaze(int size)
     {
         var maze = new Maze(size);
-        // Fill the maze with walls
-        for (int x = 0; x < size; x++)
-        {
-            for (int y = 0; y < size; y++)
-            {
-                maze.Set(x, y, true);
-            }
-        }
-
         // BSP algorithm
 
-        var x0 = 50;
+        SliceMaze(maze);
 
 
 
---
+++ Assets/Src/Maze/BSP/Particle.cs
@@ -0,0 +1,15 @@
+class Particle
+{
+    public int x0;
+    public int x1;
+    public int y0;
+    public int y1;
+
+    public Particle(int x0, int x1, int y0, int y1)
+    {
+        this.x0 = x0;
+        this.x1 = x1;
+        this.y0 = y0;
+        this.y1 = y1;
+    }
+}
